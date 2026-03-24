ac356152ca0e9eeca5cfd3adf55ea1c311052cad
ultraelectronica <fyketonel@gmail.com>
2026-03-25 02:04:03 +0800

    Changed the version on settings screen.

--- lib/features/settings/screens/settings_screen.dart
+++ lib/features/settings/screens/settings_screen.dart
@@ -359,7 +359,7 @@ class _SettingsScreenState extends ConsumerState<SettingsScreen> {
           ),
           const SizedBox(height: 4),
           const Text(
-            'Version 1.0.0',
+            'Version 0.9.1-beta.3',
             style: TextStyle(
               fontFamily: 'ProductSans',
               fontSize: 14,

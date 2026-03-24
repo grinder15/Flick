4393bb26f106d66817496baa5d81221e1d15f9ae
ultraelectronica <fyketonel@gmail.com>
2026-03-25 02:06:03 +0800

    Nakalimutan ko yung sa isa sa settings screen lol

--- lib/features/settings/screens/settings_screen.dart
+++ lib/features/settings/screens/settings_screen.dart
@@ -633,7 +633,7 @@ SOFTWARE.
                             context,
                             icon: LucideIcons.info,
                             title: 'About Flick Player',
-                            subtitle: 'Version 1.0.0',
+                            subtitle: 'Version 0.9.1-beta.3',
                             onTap: _showAboutBottomSheet,
                           ),
                           _buildDivider(),

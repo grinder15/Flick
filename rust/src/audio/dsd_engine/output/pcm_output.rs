42b4e806a881eb47e2437d911514af25331fec15
ultraelectronica <fyketonel@gmail.com>
2026-05-21 04:08:32 +0800

    Add PCM output module wrapping DSD decimation pipeline

---
+++ rust/src/audio/dsd_engine/output/pcm_output.rs
@@ -0,0 +1,35 @@
+use super::super::dsd::{DsdDecimationPipeline, DsdRate, FilterQuality};
+
+pub struct PcmOutput {
+    pipeline: DsdDecimationPipeline,
+}
+
+impl PcmOutput {
+    pub fn new(
+        dsd_rate: DsdRate,
+        target_rate: u32,
+        quality: FilterQuality,
+        channels: usize,
+    ) -> Self {
+        Self {
+            pipeline: DsdDecimationPipeline::new(dsd_rate, target_rate, quality, channels),
+        }
+    }
+
+    pub fn process(
+        &mut self,
+        dsd_bytes: &[u8],
+        channel_offsets: &[usize],
+        output: &mut Vec<f32>,
+    ) {
+        self.pipeline.process_bytes(dsd_bytes, channel_offsets, output);
+    }
+
+    pub fn output_rate(&self) -> u32 {
+        self.pipeline.target_pcm_rate()
+    }
+
+    pub fn reset(&mut self) {
+        self.pipeline.reset();
+    }
+}
